a96c20736ff403d391fb5383600b9340f2db0733
thioaana <i.anastasiadis@ergohellas.gr>
2023-06-05 09:29:14 +0300

    Restructure Cource6/Week and Finished Course6/Week2/Lab2

---
+++ Course6_APIs/Week2/Lab2/LittleLemonDRF/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import MenuItem
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ('title', 'price', 'inventory')
+        extra_kwargs = {'price': {'min_value': 2},
+                        'inventory': {'min_value': 0},
+                        }
