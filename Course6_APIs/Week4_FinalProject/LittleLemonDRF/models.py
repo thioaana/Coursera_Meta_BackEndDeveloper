a08a873381e91520c4ece9bb67b3d900bc995ca2
makis <54209831+thioaana@users.noreply.github.com>
2023-06-12 13:26:29 +0000

    Trying to fix Cart serializer in Week4

--- Course6_APIs/Week4_FinalProject/LittleLemonDRF/models.py
+++ Course6_APIs/Week4_FinalProject/LittleLemonDRF/models.py
@@ -7,7 +7,8 @@ class Category(models.Model):
     title = models.CharField(max_length=255, db_index=True)
 
     def __str__(self)->str:
-        return self.title
+        return f"{self.title}"
+
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
@@ -16,8 +17,9 @@ class MenuItem(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1, db_index=True)
     
     def __str__(self):
-        return self.title
+        return f"{self.title}"
     
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
@@ -28,12 +30,25 @@ class Cart(models.Model):
     class Meta:
         unique_together = ('menu_item', 'user')
 
+    def save(self, *args, **kwargs):
+        self.unit_price = self.menu_item.price
+        self.price = self.quantity * self.unit_price
+        super(Cart, self).save(*args, **kwargs)
+
+    def __str__(self)->str:
+        return f"{self.user} - {self.menu_item}"
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     delivery_crew = models.ForeignKey(User, on_delete=models.CASCADE, related_name='delivery_crew', null=True)
     status = models.BooleanField(db_index=True, default=0)
     total = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(db_index=True)
+
+    def __str__(self)->str:
+        return f"{self.user} - status:{self.status}"
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -44,3 +59,7 @@ class OrderItem(models.Model):
 
     class Meta:
         unique_together = ('order', 'menu_item')
+
+    def __str__(self)->str:
+        return f"{self.order} - {self.menu_item}"
+    
--- Course6_APIs/Week4_FinalProject/LittleLemonDRF/serializers.py
+++ Course6_APIs/Week4_FinalProject/LittleLemonDRF/serializers.py
@@ -15,7 +15,8 @@ class MenuItemSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'price', 'stock', 'category', 'category_id')
 
 class CartSerializer(serializers.ModelSerializer):
+    # price = serializers.SerializerMethodField('calculate_price')
     class Meta:
         model = Cart
         fields = ('user', 'menu_item', 'quantity', 'unit_price', 'price')
-        read_only_fields = ('user', 'unit price', 'price')
+        read_only_fields = ('user',  'price', 'unit_price')
